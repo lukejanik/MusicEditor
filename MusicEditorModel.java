c41d4fa80f1c9fb602237044f2adbd948274340c
ashnashah123 <shah.ash@husky.neu.edu>
2016-10-26 17:30:16 -0400

    Add files via upload

--- MusicEditorModel.java
+++ MusicEditorModel.java
@@ -1,128 +1,103 @@
 package cs3500.music.model;
 
-
-import java.util.ArrayList;
+import java.util.List;
 
 /**
- * This is the class for MusicEditorModel.
+ * Created by ashnashah on 10/17/16.
  */
-public final class MusicEditorModel implements IMusicEditorModel {
+
+
+// To represent a music editor.
+public class MusicEditorModel implements IMusicEditorModel {
 
   MusicEditorModel() {
-    // No parameters.
+    // empty constructor.
   }
 
   /**
-   * Adds the note at its beat number to the given piece.
-   * If the beat number does not exist in the piece, the note is still added to the piece.
-   * If the beat number does exist in the piece, the note is added to that beat and all beats
-   * within the duration of the note.
-   *
-   * @param note  The note to add to the piece
-   * @param piece The piece to add the note to
+   * @param sym   note that will be written.
+   * @param piece piece that the note will be written to.
+   * @return Piece of music that the notes will be written to.
    */
   @Override
-  public void write(Playable note, Piece piece) {
-    piece.addNote(note);
+  public Piece writeMusic(AMusicSymbol sym, Piece piece) {
+    piece.addSym(sym);
+    return piece;
   }
 
+  /**
+   * To replace an old note with a new note.
+   *
+   * @param oldSym the note that is to be replaced.
+   * @param newSym the note that the oldNote will be replaced with.
+   * @param piece  the piece that is to be edited. -- pass a list<Pieces> </Pieces>
+   */
   @Override
-  public void edit(Playable noteToReplace, Playable noteToAdd, Piece piece) {
-    if (noteToAdd.getBeatNumber() != noteToReplace.getBeatNumber()) {
-      throw new IllegalArgumentException("Invalid edit: beat numbers of both notes must match.");
+  public void edit(AMusicSymbol oldSym, AMusicSymbol newSym, Piece piece) {
+    piece.editSym(oldSym, newSym);
+  }
+
+  /**
+   * To remove a given note from the given piece.
+   *
+   * @param sym   note to be removed.
+   * @param piece piece that the note will be removed from.
+   */
+  @Override
+  public void remove(AMusicSymbol sym, Piece piece) throws IllegalArgumentException {
+    if (piece.validPiece()) {
+      piece.removeSym(sym);
     } else {
-      piece.removeNote(noteToReplace);
-      piece.addNote(noteToAdd);
+      throw new IllegalArgumentException("Invalid piece");
     }
   }
 
+  /**
+   * To combine a list of pieces either simultaneously or consecutively.
+   *
+   * @param type   The type of combination you want to make (simultaneous or consecutive).
+   * @param pieces The list of pieces that are to be combined.
+   */
   @Override
-  public void remove(Playable note, Piece piece) {
-    piece.removeNote(note);
+  public Piece combine(CombineType type, List<Piece> pieces) {
+    switch (type) {
+      case CONSECUTIVE:
+        Piece consecPiece = new Piece();
+        int lengthOfPiece = 0;
+        for (Piece p : pieces) {
+          lengthOfPiece = lengthOfPiece + p.size();
+        }
+        consecPiece.addBeats(lengthOfPiece);
+        consecPiece.appendPieces(pieces);
+        return consecPiece;
+
+      case SIMULTANEOUS:
+        int maxLength = 0;
+        for (Piece p : pieces) {
+          if (p.size() > maxLength) {
+            maxLength = p.size();
+          }
+        }
+        Piece simulPiece = new Piece();
+        simulPiece.addBeats(maxLength);
+        for (Piece p : pieces) {
+          simulPiece.addMusicSymbols(p);
+        }
+        return simulPiece;
+      default:
+        throw new IllegalArgumentException("Invalid combination type");
+    }
   }
 
+
+  /**
+   * To return a string of the editorState of the given piece.
+   *
+   * @param piece The piece that is part of the music editor.
+   * @return String of the editorState of the given piece.
+   */
   @Override
-  public Piece combine(ArrayList<Piece> piecesToCombine, CombineType type) {
-    if (type.equals(CombineType.SEQUENTIAL)) {
-      return combineSequential(piecesToCombine);
-    }
-    if (type.equals(CombineType.SIMULTANEOUS)) {
-      return combineSimulteaneous(piecesToCombine);
-    } else {
-      throw new IllegalArgumentException("Invalid Combination Type.");
-    }
-  }
-
-  /**
-   * Add the beats one at a time from each piece, in order.
-   *
-   * @param piecesToCombine An arraylist of the pieces to combine.
-   * @return Piece a new combined piece.
-   */
-  private Piece combineSequential(ArrayList<Piece> piecesToCombine) {
-    Piece combo = new Piece();
-
-    // Add the first
-    for (Beat beat : piecesToCombine.get(0).beats) {
-      for (Playable p : beat.notes) {
-        combo.addNote(p);
-      }
-    }
-    int size = piecesToCombine.get(0).beats.size();
-    for (int i = 1; i < piecesToCombine.size(); i++) {
-      for (Beat beat : piecesToCombine.get(i).beats) {
-        for (Playable p : beat.notes) {
-          Playable n = new Note(p.getPitch(), p.getOctave(), p.getDuration(), p.getBeatNumber() +
-                  size);
-          combo.addNote(n);
-        }
-      }
-      size += piecesToCombine.get(i).beats.size();
-    }
-
-    return combo;
-  }
-
-  /**
-   * Add the beats simultaneously.
-   *
-   * @param piecesToCombine An arraylist of the pieces to be combined.
-   * @return Piece a new combined piece.
-   */
-  private Piece combineSimulteaneous(ArrayList<Piece> piecesToCombine) {
-    int max = 0;
-    for (Piece piece : piecesToCombine) {
-      if (piece.beats.size() > max) {
-        max = piece.beats.size();
-      }
-    }
-    ArrayList<Beat> comboBeats = new ArrayList<>();
-    for (int i = 0; i < max; i++) {
-      comboBeats.add(new Beat());
-    }
-    Piece combo = new Piece();
-    combo.beats = comboBeats;
-
-    for (Piece piece : piecesToCombine) {
-      for (int i = 0; i < piece.beats.size(); i++) {
-        for (Playable note : piece.beats.get(i).notes) {
-          combo.addNote(note);
-        }
-      }
-    }
-    return combo;
-  }
-
-  /**
-   * Returns a string which is the current "state" of the music editor.
-   *
-   * @param piece one piece to display to the output.
-   * @return String
-   */
   public String getEditorState(Piece piece) {
-    String state = "";
-    state += piece.getState();
-    return state;
+    return piece.setUpState();
   }
-
 }
